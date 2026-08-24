0defc42d7ad74bc35bcf1146d6a11f1c277df9d7
PRADEEP PATEL <pradeep4ies@gmail.com>
2026-08-24 23:44:34 +0530

    feat: set up Express server and add initial middleware; create db configuration file

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const PORT= 4000;
+const app= express();
+
+
+//Middlewares
+app.use(cors());
+app.use(express.json({limit:"1mb"}));
+
+
+//DB
+//Routes
+app.get('/', (req,res)=>{
+    res.send("API WORKING");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server Started on http://localhost:${PORT}`);
+})
