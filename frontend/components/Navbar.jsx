bea23cb022ed9d367bde64045413c8cc351fdfe7
PRADEEP PATEL <pradeep4ies@gmail.com>
2026-09-07 23:30:18 +0530

    feat: add empty div for future content in Navbar component

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar=()=>{
         <nav className={s.root}>
             <div className={s.container}>
                 <Logo/>
+                <div></div>
             </div>
         </nav>
     )
